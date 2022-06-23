cc0d668a5dfb78be6e8e5d85df5e1f4b412a6b63
Raquibul024 <73742694+Raquibul024@users.noreply.github.com>
2022-06-23 22:53:10 +0600

    Add files via upload

---
+++ OOP_024_V.0.1/Calculator.java
@@ -0,0 +1,84 @@
+package calculator;
+
+import static java.lang.System.exit;
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        double x, y;
+        Calculations c = new Calculations();
+        while (true) {
+            System.out.println("Operations:");
+            System.out.print("\n1.Summation\n2.Subtraction\n3.Multiplication\n4.Division\n5.Root\n6.Modulas");
+            System.out.print("\n7.Max value\n8.Min value\n9.Sine OP\n10.Cosine OP\n11.Exit");
+            
+            System.out.print("\n\nEnter your choice: ");
+            int ch = s.nextInt();
+
+            switch (ch) {
+                case 1:
+                    System.out.print("\nEnter value of x and y: ");
+                    x = s.nextDouble();
+                    y = s.nextDouble();
+                    c.sum(x, y);
+                    break;
+                case 2:
+                    System.out.print("\nEnter value of x and y: ");
+                    x = s.nextDouble();
+                    y = s.nextDouble();
+                    c.sub(x, y);
+                    break;
+                case 3:
+                    System.out.print("\nEnter value of x and y: ");
+                    x = s.nextDouble();
+                    y = s.nextDouble();
+                    c.mult(x, y);
+                    break;
+                case 4:
+                    System.out.print("\nEnter value of x and y: ");
+                    x = s.nextDouble();
+                    y = s.nextDouble();
+                    c.div(x, y);
+                    break;
+                case 5:
+                    System.out.print("\nEnter value of x: ");
+                    x = s.nextDouble();
+                    c.root(x);
+                    break;
+                case 6:
+                    System.out.print("\nEnter value of x and y: ");
+                    x = s.nextDouble();
+                    y = s.nextDouble();
+                    c.mod(x, y);
+                    break;
+                case 7:
+                    System.out.print("\nEnter value of x and y: ");
+                    x = s.nextDouble();
+                    y = s.nextDouble();
+                    c.max(x, y);
+                    break;
+                case 8:
+                    System.out.print("\nEnter value of x and y: ");
+                    x = s.nextDouble();
+                    y = s.nextDouble();
+                    c.min(x, y);
+                    break;
+                case 9:
+                    System.out.print("\nEnter value of x: ");
+                    x = s.nextDouble();
+                    c.sin(x);
+                    break;
+                case 10:
+                    System.out.print("\nEnter value of x: ");
+                    x = s.nextDouble();
+                    c.cos(x);
+                    break;
+                case 11:
+                    exit(0);
+            }
+
+        }
+    }
+}
