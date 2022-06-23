5a88f8d3d48084c5869c55d86dc1d3490703173c
Raquibul024 <73742694+Raquibul024@users.noreply.github.com>
2022-06-23 22:41:20 +0600

    Add files via upload

---
+++ OOP_024_V.0.1/Calculations.java
@@ -0,0 +1,56 @@
+package calculator;
+
+public class Calculations {
+
+    void sum(double x, double y) {
+        System.out.println("Summation of " + x + " and " + y + " is: " + (x + y));
+        System.out.println("\n\n--------------------------------------------------------------\n\n");
+    }
+
+    void sub(double x, double y) {
+        System.out.println("Subtraction of " + x + " and " + y + " is: " + (x - y));
+        System.out.println("\n\n--------------------------------------------------------------\n\n");
+    }
+
+    void mult(double x, double y) {
+        System.out.println("Multiplication of " + x + " and " + y + " is: " + (x * y));
+        System.out.println("\n\n--------------------------------------------------------------\n\n");
+    }
+
+    void div(double x, double y) {
+        System.out.println("Division of " + x + " and " + y + " is: " + (x / y));
+        System.out.println("\n\n--------------------------------------------------------------\n\n");
+    }
+
+    void mod(double x, double y) {
+        System.out.println("Modulas of " + x + " and " + y + " is: " + (x % y));
+        System.out.println("\n\n--------------------------------------------------------------\n\n");
+    }
+
+    void root(double x) {
+        System.out.println("Root value of " + x + " is: " + (Math.sqrt(x)));
+        System.out.println("\n\n--------------------------------------------------------------\n\n");
+    }
+
+    void max(double x, double y) {
+        System.out.println("Maximum value between " + x + " and " + y + " is: " + (Math.max(x, y)));
+        System.out.println("\n\n--------------------------------------------------------------\n\n");
+    }
+
+    void min(double x, double y) {
+        System.out.println("Minimum value between " + x + " and " + y + " is: " + (Math.min(x, y)));
+        System.out.println("\n\n--------------------------------------------------------------\n\n");
+    }
+
+    void sin(double x) {
+        double a = Math.toRadians(x);
+        System.out.println("sin" + x + " is: " + (Math.sin(a)));
+        System.out.println("\n\n--------------------------------------------------------------\n\n");
+    }
+
+    void cos(double x) {
+        double a = Math.toRadians(x);
+        System.out.println("cos" + x + " is: " + (Math.cos(a)));
+        System.out.println("\n\n--------------------------------------------------------------\n\n");
+    }
+}
